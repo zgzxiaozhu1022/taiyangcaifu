943c75ca33ae356f84f8a3cd345379e7e630a187
朱国政 <zhuguozheng@zhuguozhengdeMac-mini.local>
2017-08-11 11:04:38 +0800

    项目初始化

---
+++ taiyangcaifu/taiyangcaifu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  taiyangcaifu
+//
+//  Created by 朱国政 on 2017/8/11.
+//  Copyright © 2017年 朱国政. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ taiyangcaifu/taiyangcaifu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  taiyangcaifu
+//
+//  Created by 朱国政 on 2017/8/11.
+//  Copyright © 2017年 朱国政. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
